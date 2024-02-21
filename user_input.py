c5823f566f41ef63bfcaa6b703d5fe34d91f750b
Engel Otieno <engelstan0@gmail.com>
2024-02-21 12:11:01 +0300

    Personal_Data

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name=input('What is Your Name: ')
+age=input('What is your age: ')
+location=input('What is your location: ')
+u=f'Hello {name}, you are {age} years old and live in {location}'
+print(u)
