d10f9c70f130756a3a6f46c06bf731ffaa2ee1ef
Engel Otieno <engelstan0@gmail.com>
2024-02-22 21:31:27 +0300

    Variables_Basics

---
+++ variables.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Variables
+  // String- Storing Text Value
+  // int - storing int value
+  String name = 'Engel Otieno';
+  String address = 'Migosi';
+  int age = 76;
+  double price = 100.98;
+  var year = 2024;
+  num students = 460;
+
+  print(name);
+  print(address);
+  print(age);
+  print(price);
+  print(year);
+  print(students);
+}
