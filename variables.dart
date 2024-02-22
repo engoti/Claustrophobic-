d6a6c23b25753069dcda81bb4d2dccbabe01d3b3
engoti <136898202+engoti@users.noreply.github.com>
2024-02-22 21:35:33 +0300

    Basics_Variables

--- variables.dart
+++ variables.dart
@@ -2,6 +2,9 @@ void main() {
   // Variables
   // String- Storing Text Value
   // int - storing int value
+  // double - storing float value
+  // var - storing any value type
+  // num - storing any value of type (number)
   String name = 'Engel Otieno';
   String address = 'Migosi';
   int age = 76;
