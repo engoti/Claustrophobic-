a50cae52044463a8733857892c8626053ead331a
Engel Otieno <engelstan0@gmail.com>
2024-03-13 22:26:40 +0300

    Control Flow in Dart

---
+++ Control_Flow.dart
@@ -0,0 +1,10 @@
+void main() {
+  var number = 20;
+  if (number > 10) {
+    print("Your Number is Greater than 10");
+  } else if(number < 10) {
+    print("Your Number is Less than 10");
+  } else if(number == 10) {
+    print("Your Number is Equal to 10");
+  }
+}
